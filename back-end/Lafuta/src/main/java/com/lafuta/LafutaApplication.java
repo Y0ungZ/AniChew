9ab43177d72feccf5645b87ba6f923848f4bbe5b
shining8543 <shining8543@naver.com>
2021-09-08 16:56:21 +0900

    setup:[#S05P21B304-335] BE Spring Setup

---
+++ back-end/Lafuta/src/main/java/com/lafuta/LafutaApplication.java
@@ -0,0 +1,13 @@
+package com.lafuta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LafutaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LafutaApplication.class, args);
+	}
+
+}
---
+++ back-end/Lafuta/src/test/java/com/lafuta/LafutaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lafuta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LafutaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
