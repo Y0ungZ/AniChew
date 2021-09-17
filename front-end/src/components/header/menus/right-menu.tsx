f1e3bd380e5b339c5ef05401bdaa44002a99c67e
rolancia <95jjangjun@gmail.com>
2021-09-17 14:49:19 +0900

    feat: [#S05P21B304-530] 로그인 버튼에 로그인 모달 연결

--- front-end/src/components/header/menus/right-menu.tsx
+++ front-end/src/components/header/menus/right-menu.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from 'antd';
 import { BellFilled, SearchOutlined } from '@ant-design/icons';
+import LoginModal from '../../modal/login-modal/login-modal';
 
-const RightMenu = () => (
-  <>
-    <Button icon={<SearchOutlined />} type="link" />
-    <Button icon={<BellFilled />} type="link" />
-    <Button type="link">로그인</Button>
-  </>
-);
+const RightMenu = () => {
+  const [visible, setVisible] = useState(false);
+
+  return (
+    <>
+      <Button icon={<SearchOutlined />} type="link" />
+      <Button icon={<BellFilled />} type="link" />
+      <Button type="link" onClick={() => setVisible(true)}>로그인</Button>
+      <LoginModal visible={visible} setVisible={setVisible} />
+    </>
+  );
+};
 
 export default RightMenu;
