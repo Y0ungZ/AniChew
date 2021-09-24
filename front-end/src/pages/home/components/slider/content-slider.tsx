1e5bd4d6d848d5fa50ec97b18ca3f187f11b397b
노영주 <at8in@naver.com>
2021-09-24 13:59:50 +0900

    feat:[#S05P21B304-614] 컨텐츠 슬라이더

---
+++ front-end/src/pages/home/components/slider/content-slider.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Typography } from 'antd';
+import Slider, { Settings } from 'react-slick';
+import ContentSliderItem from './content-slider-item';
+import { AnimeList } from '../../../../types/anime-list-type';
+import { CssKeyObject } from '../../../../models/css-basic-type';
+import '../../../../assets/css/color.css';
+
+const { Title } = Typography;
+
+type ContentItemProps = {
+  datas: AnimeList[],
+  title: string
+};
+
+const styles: CssKeyObject = {
+  position: {
+    marginLeft: '2em',
+    paddingTop: '2em',
+    paddingBottom: '2em',
+  },
+  title: {
+    fontFamily: 'titleFont',
+    color: 'var(--text-dark)',
+  },
+};
+
+const settings: Settings = {
+  infinite: false,
+  slidesToShow: 5,
+  slidesToScroll: 5,
+  initialSlide: 0,
+  responsive: [
+    {
+      breakpoint: 769,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+      },
+    },
+    {
+      breakpoint: 577,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+  ],
+};
+
+const ContentSlider = (props:ContentItemProps) => (
+
+  <div style={styles.position}>
+    <Title style={styles.title} level={3}>
+      {props.title}
+    </Title>
+    <Slider {...settings}>
+      {props.datas.map((data) => (
+        <ContentSliderItem key={data.animeId} data={data} />
+      ))}
+    </Slider>
+  </div>
+);
+
+export default ContentSlider;
