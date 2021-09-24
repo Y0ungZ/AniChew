56111474a7cba9c148e143c03228c96539295c28
노영주 <at8in@naver.com>
2021-09-24 13:59:40 +0900

    feat:[#S05P21B304-597] 프로모션 캐러셀 컴포넌트

---
+++ front-end/src/pages/home/components/carousel/promotion-carousel-item.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { Button } from 'antd';
+import { StarFilled } from '@ant-design/icons';
+import { AnimeList } from '../../../../types/anime-list-type';
+import { CssKeyObject } from '../../../../models/css-basic-type';
+import '../../../../assets/css/color.css';
+
+type ContentItemProps = {
+  data: AnimeList,
+};
+
+const styles: CssKeyObject = {
+  title: {
+    fontFamily: 'logoFont',
+    color: 'var(--main-color)',
+    fontSize: '2em',
+    margin: '0',
+  },
+  synopsis: {
+    color: 'white',
+    width: '80vw',
+    display: '-webkit-box',
+    textOverflow: 'ellipsis',
+    overflow: 'hidden',
+    WebkitLineClamp: 4,
+    WebkitBoxOrient: 'vertical',
+  },
+  detail: {
+    color: 'var(--main-color)',
+  },
+};
+
+const PromotionCarouselItem = (props :ContentItemProps) => (
+  <div style={{
+    background: `linear-gradient(
+      rgba(20, 20, 20, 1) 0%,
+      rgba(20, 20, 20, 0.3) 50%,
+      rgba(20, 20, 20, 1) 100%
+      ),
+      url(${process.env.REACT_APP_IMAGE_BASE_URL
+      }anime_imgs/${props.data.animeId}.jpg) center/cover`,
+    backgroundSize: 'cover',
+    height: '30em',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }}
+  >
+    <div>
+      <span style={styles.title}>
+        {props.data.animeKoreanName}
+      </span>
+      <p style={styles.synopsis}>
+        {props.data.animeSynopsis}
+      </p>
+      <p style={styles.detail}>
+        {props.data.animeGenres.map((genre) => (
+          <span key={genre}>
+            {genre}
+            |
+          </span>
+        ))}
+        <span>
+          <StarFilled />
+          {props.data.animeRating}
+        </span>
+      </p>
+      <Button type="primary" ghost>자세히보기</Button>
+    </div>
+  </div>
+);
+
+export default PromotionCarouselItem;
---
+++ front-end/src/pages/home/components/carousel/promotion-carousel.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Carousel } from 'antd';
+import PromotionCarouselItem from './promotion-carousel-item';
+import { CssKeyObject } from '../../../../models/css-basic-type';
+import '../../../../assets/css/color.css';
+
+const styles: CssKeyObject = {
+  carousel: {
+    height: '30em',
+    textAlign: 'center',
+    backgroundColor: 'var(--text-dark)',
+    overflow: 'hidden',
+  },
+};
+
+const datas = [
+  {
+    animeId: 1535,
+    animeName: 'Death Note',
+    animeGenres: ['미스테리', '초능력', '스릴러'],
+    animeJapaneseName: 'デスノート',
+    animeKoreanName: '데스 노트',
+    animeStudios: ['Madhouse'],
+    animeRating: 8.63,
+    animeSynopsis:
+      '평범한 고교생인 야가미 라이토. 그러던 어느 날 하늘에서 검은 노트가 라이토 앞에 떨어진다. 그 노트는 사신계에서 떨어진 죽음의 노트. 그 노트에 이름이 적힌 자는 반드시 죽게 되는 노트인 데스노트였다.',
+  }, {
+    animeId: 3786,
+    animeName: 'Evangelion: 3.0+1.0 Thrice Upon a Time',
+    animeGenres: ['액션', '드라마', 'SF'],
+    animeJapaneseName: 'シン・エヴァンゲリオン劇場版:||',
+    animeKoreanName: '신 에반게리온 극장판:∥',
+    animeStudios: ['Khara'],
+    animeRating: 8.72,
+    animeSynopsis:
+      'さらば、全てのエヴァンゲリオン。 안녕, 모든 에반게리온. 에반게리온 신극장판 시리즈의 4번째 작품이자 최종장. 전작의 개봉 이후 9년 만의 개봉. 25년 동안 달려온 에반게리온, 안녕!',
+  },
+];
+
+const PromotionCarousel = () => (
+  <div>
+    <Carousel style={styles.carousel} autoplay autoplaySpeed={6000}>
+      {datas.map((data) => (
+        <PromotionCarouselItem key={data.animeId} data={data} />
+      ))}
+    </Carousel>
+  </div>
+);
+
+export default PromotionCarousel;
