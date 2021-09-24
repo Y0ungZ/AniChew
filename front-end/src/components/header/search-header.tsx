be5085256ece3a0d1b32e8b78f588bf335dad2bb
노영주 <at8in@naver.com>
2021-09-24 11:41:13 +0900

    feat:[#S05P21B304-608] 검색헤더 컴포넌트 추가

---
+++ front-end/src/components/header/search-header.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Input } from 'antd';
+import { CssKeyObject } from '../../models/css-basic-type';
+import '../../assets/css/color.css';
+
+const { Search } = Input;
+
+type searchOpen = {
+    open: boolean;
+};
+
+const styles: CssKeyObject = {
+  displayBlock: {
+    display: 'block',
+    textAlign: 'center',
+  },
+  displayNone: {
+    display: 'none',
+  },
+  header: {
+    backgroundImage: 'linear-gradient(white,var(--background-light))',
+    height: '10em',
+  },
+  searchInput: {
+    marginTop: '4em',
+    width: '70%',
+  },
+};
+
+const SearchHeader = ({ open }: searchOpen) => {
+  const onSearch = (value: string) => {
+    console.log(value);
+  };
+
+  return (
+    <div style={open ? styles.displayBlock : styles.displayNone}>
+      {open && (
+        <div style={styles.header}>
+          <Search
+            placeholder="작품, 캐릭터를 검색해보세요."
+            style={styles.searchInput}
+            enterButton
+            onSearch={onSearch}
+            size="large"
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SearchHeader;
