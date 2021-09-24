32eb26295590c86ed1d6a2a856d9ea714d6042e4
노영주 <at8in@naver.com>
2021-09-24 11:41:55 +0900

    feat:[#S05P21B304-610] 헤더 검색관련 추가

--- front-end/src/components/header/menus/right-menu.tsx
+++ front-end/src/components/header/menus/right-menu.tsx
@@ -3,12 +3,20 @@ import { Button } from 'antd';
 import { BellFilled, SearchOutlined } from '@ant-design/icons';
 import LoginModal from '../../modal/login-modal/login-modal';
 
-const RightMenu = () => {
+type toggleSearchProps = {
+  toggleSearchHeader: ()=>void;
+}
+
+const RightMenu = (props:toggleSearchProps) => {
   const [visible, setVisible] = useState(false);
+
+  const toggleSearchHeader = () => {
+    props.toggleSearchHeader();
+  };
 
   return (
     <>
-      <Button icon={<SearchOutlined />} type="link" />
+      <Button icon={<SearchOutlined />} type="link" onClick={toggleSearchHeader} />
       <Button icon={<BellFilled />} type="link" />
       <Button type="link" onClick={() => setVisible(true)}>로그인</Button>
       <LoginModal visible={visible} setVisible={setVisible} />
