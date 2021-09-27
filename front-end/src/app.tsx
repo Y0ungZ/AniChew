f59a431ddefb496bb4585b5c10bd582ffc03d0e9
rolancia <95jjangjun@gmail.com>
2021-09-27 11:22:20 +0900

    feat: [#S05P21B304-745] refresh save

--- front-end/src/app.tsx
+++ front-end/src/app.tsx
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './app.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Layout } from 'antd';
 import { routes } from './routes/config';
 import Router from './routes/router';
 import MainHeader from './components/header/header';
+import { mainAxios } from './libs/axios';
+import { useAuth, useUser } from './hooks';
 
-export const user = {
-  authenticated: false,
+const App = () => {
+  const auth = useAuth();
+  const user = useUser();
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      mainAxios.defaults.headers.common.Authorization = token;
+      auth.isLoggedIn = true;
+      user.me();
+    }
+  });
+
+  return (
+    <Layout>
+      <BrowserRouter>
+        <MainHeader />
+        <Router routes={routes} />
+      </BrowserRouter>
+    </Layout>
+  );
 };
-
-const App = () => (
-  <Layout>
-    <BrowserRouter>
-      <MainHeader />
-      <Router routes={routes} />
-    </BrowserRouter>
-  </Layout>
-);
 
 export default App;
