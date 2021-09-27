685b489683c7c37562617630d4028a70d866a9c6
rolancia <95jjangjun@gmail.com>
2021-09-27 11:22:59 +0900

    feat: [#S05P21B304-746] isLoggedIn 사용

--- front-end/src/routes/route-with-sub-routes.tsx
+++ front-end/src/routes/route-with-sub-routes.tsx
@@ -1,23 +1,25 @@
 /* eslint-disable no-nested-ternary */
 import React, { Suspense } from 'react';
 import { Redirect, Route } from 'react-router-dom';
+import { observer } from 'mobx-react';
 import { RouteType } from './config';
-import { user } from '../app';
+import { useAuth } from '../hooks';
 
-const RouteWithSubRoutes = (route: RouteType) => {
-  const { authenticated } = user;
+const RouteWithSubRoutes = observer((route: RouteType) => {
+  const auth = useAuth();
+  const { isLoggedIn } = auth;
   return (
     <Suspense fallback={route.fallback}>
       <Route
         path={route.path}
         render={(props) => (route.redirect ? <Redirect to={route.redirect} /> :
           route.private ? (
-            authenticated ? route.component &&
+            isLoggedIn ? route.component &&
             <route.component {...props} routes={route.routes} /> : <Redirect to='/' />
           ) : route.component && <route.component {...props} routes={route.routes} />)}
       />
     </Suspense>
   );
-};
+});
 
 export default RouteWithSubRoutes;
