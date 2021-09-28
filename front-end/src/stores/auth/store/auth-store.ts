b0c19fa2f104bf3b63c8dc0034c7be3dc8e91ddc
노영주 <at8in@naver.com>
2021-09-28 10:56:50 +0900

    fix:[#S05P21B304-752] right-menu observer추가,store수정

--- front-end/src/stores/auth/store/auth-store.ts
+++ front-end/src/stores/auth/store/auth-store.ts
@@ -1,4 +1,4 @@
-import { makeAutoObservable } from 'mobx';
+import { makeAutoObservable, runInAction } from 'mobx';
 import authRepository from '../repository/auth-repository';
 
 export default class AuthStore {
@@ -11,7 +11,9 @@ export default class AuthStore {
   async login(authCode: string) {
     const res = await authRepository.login(authCode);
     if (res.status === 200) {
-      this.isLoggedIn = true;
+      runInAction(() => {
+        this.isLoggedIn = true;
+      });
     } else {
       throw new Error('로그인 실패');
     }
@@ -20,7 +22,9 @@ export default class AuthStore {
   async logout() {
     const res = await authRepository.logout();
     if (res.status === 200) {
-      this.isLoggedIn = false;
+      runInAction(() => {
+        this.isLoggedIn = false;
+      });
     } else {
       throw new Error('로그아웃 실패');
     }
