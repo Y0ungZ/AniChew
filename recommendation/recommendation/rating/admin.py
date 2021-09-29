5103117d614b63c2cd6f91da3d97590e3bd9e41d
muni <ansgml6491@naver.com>
2021-09-29 10:28:29 +0900

    feat: [S05P21B304-591] 장고 어드민 생성

--- recommendation/recommendation/rating/admin.py
+++ recommendation/recommendation/rating/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import RatingCompleted, SimilarAnimes, RecommendataionCF
 
 # Register your models here.
+admin.site.register(RatingCompleted)
+admin.site.register(SimilarAnimes)
+admin.site.register(RecommendataionCF)
