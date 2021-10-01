b011cdb4cf79a7401b070ceb0a7bdd22ccf820a7
rolancia <95jjangjun@gmail.com>
2021-10-01 14:31:49 +0900

    feat: [#S05P21B304-823] review form, read form
    Former-commit-id: 0353c4eab300c151ec0b1aac1d863d979499d767

---
+++ front-end/src/pages/ani-detail/components/form/review-form/review-form.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { observer } from 'mobx-react';
+import { Form, Input, Button } from 'antd';
+import { CssKeyObject } from '../../../../../types/css-basic-type';
+import { useAni } from '../../../../../hooks';
+
+const styles: CssKeyObject = {
+  submitBtn: {
+    float: 'right', marginTop: '1em',
+  },
+  cancelBtn: {
+    float: 'right', marginTop: '1em', marginRight: '1em',
+  },
+};
+
+const ReviewForm = observer(({ id }: {id: string}) => {
+  const ani = useAni();
+
+  const submitReview = ({ review }: {review: string}) => {
+    console.log(review);
+    ani.submitReview(id, review)
+      .then()
+      .catch(() => alert('리뷰 남기기 실패!'));
+  };
+
+  const cancelUpdate = () => {
+    ani.reviewFormMode = 'Read';
+  };
+
+  return (
+    <Form onFinish={submitReview}>
+      <Form.Item
+        name="review"
+        rules={[
+          {
+            required: true,
+            message: '리뷰를 작성해주세요.',
+          },
+        ]}
+      >
+        <Input.TextArea
+          name="content"
+          placeholder={ani.myReview ? '' : '100자 이내'}
+          showCount
+          maxLength={100}
+          defaultValue={ani.myReview ? ani.myReview.content : ''}
+        />
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary" htmlType="submit" style={styles.submitBtn}>
+          작성
+        </Button>
+        {ani.reviewFormMode === 'Update' && (
+        <Button onClick={cancelUpdate} style={styles.cancelBtn}>
+          취소
+        </Button>
+        )}
+      </Form.Item>
+    </Form>
+  );
+});
+
+export default ReviewForm;
