97719c062deb5ebf50f232f8a1d85c862bcd75de
rolancia <95jjangjun@gmail.com>
2021-10-01 14:26:04 +0900

    feat: [#S05P21B304-820] review logic
    Former-commit-id: a92a3ad6bad8e4c05f56b6961734028eb1be6b84

--- front-end/src/stores/ani/repository/ani-repository.ts
+++ front-end/src/stores/ani/repository/ani-repository.ts
@@ -6,6 +6,27 @@ class AniRepository {
   getAniDetailInfo(animeId: string) {
     return mainAxios.get(`${this.url}/anime/${animeId}`);
   }
+
+  writeReview(animeId: string, content: string) {
+    return mainAxios.post(`${this.url}/anime/${animeId}/review`, {
+      content,
+    });
+  }
+
+  updateReview(animeId: string, content: string, id: string) {
+    return mainAxios.put(`${this.url}/anime/${animeId}/review`, {
+      id,
+      content,
+    });
+  }
+
+  deleteReview(animeId: string, id: string) {
+    return mainAxios.delete(`${this.url}/anime/${animeId}/review/${id}`);
+  }
+
+  getMyReview(animeId: string) {
+    return mainAxios.get(`${this.url}/anime/${animeId}/review`);
+  }
 }
 
 export default new AniRepository(process.env.REACT_APP_API_DOMAIN_URL!);
