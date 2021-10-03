22f0b38eed0afbbc94e30f4287112dd355aaae89
rolancia <95jjangjun@gmail.com>
2021-10-04 00:23:30 +0900

    refactor: [#S05P21B304-869] ani repository
    Former-commit-id: 0f32045a8b523e038ed33aa44e016eb0ebe9454f

--- front-end/src/stores/ani/repository/ani-repository.ts
+++ front-end/src/stores/ani/repository/ani-repository.ts
@@ -3,29 +3,26 @@ import { mainAxios } from '../../../libs/axios';
 class AniRepository {
   constructor(private readonly url: string) {}
 
-  getAniDetailInfo(animeId: string) {
-    return mainAxios.get(`${this.url}/anime/${animeId}`);
+  getInfo(id: string) {
+    return mainAxios.get(`${this.url}/anime/${id}`);
   }
 
-  setAniScore(animeId: string, score: number) {
-    return mainAxios.post(
-      `${process.env.REACT_APP_API_DOMAIN_URL}/anime/${animeId}/score`,
-      { score: score * 2 },
-    );
+  setScore(id: string, score: number) {
+    return mainAxios.post(`${this.url}/anime/${id}/score`, {
+      score: score * 2,
+    });
   }
 
-  deleteAniScore(animeId: string) {
-    return mainAxios.delete(
-      `${process.env.REACT_APP_API_DOMAIN_URL}/anime/${animeId}/score`,
-    );
+  removeScore(id: string) {
+    return mainAxios.delete(`${this.url}/anime/${id}/score`);
   }
 
-  setFavoriteAni(animeId: string) {
-    return mainAxios.post(`${this.url}/anime/${animeId}/favorite`);
+  like(id: string) {
+    return mainAxios.post(`${this.url}/anime/${id}/favorite`);
   }
 
-  deleteFavoriteAni(animeId: string) {
-    return mainAxios.delete(`${this.url}/anime/${animeId}/favorite`);
+  cancelLike(id: string) {
+    return mainAxios.delete(`${this.url}/anime/${id}/favorite`);
   }
 }
 
