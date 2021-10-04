33acfe22af7b6c6ab7b6e1bb8bbb8af06349e28a
rolancia <95jjangjun@gmail.com>
2021-10-04 22:36:49 +0900

    fix: [#S05P21B304-942] string-template
    Former-commit-id: eb1a63c50efe3e27279d1d2686ed64aefffd304c

--- front-end/src/common/string-template/string-template.ts
+++ front-end/src/common/string-template/string-template.ts
@@ -12,6 +12,7 @@ export const FAIL_WRITE_REVIEW = '리뷰 작성에 실패하셨습니다.';
 export const FAIL_UPDATE_REVIEW = '리뷰 수정에 실패하셨습니다.';
 export const FAIL_DELETE_REVIEW = '리뷰 삭제에 실패하셨습니다.';
 export const FAIL_GET_MY_REVIEW = '나의 리뷰 가져오기에 실패하셨습니다.';
+export const FAIL_GET_ALL_REVIEW = '리뷰 목록 가져오기에 실패하셨습니다.';
 
 export const FAIL_LIKE_REVIEW = '리뷰 좋아요에 실패하셨습니다.';
 export const FAIL_CANCEL_LIKE_REVIEW = '리뷰 좋아요 취소에 실패하셨습니다.';
@@ -24,3 +25,5 @@ export const FAIL_CANCEL_LIKE_ANI = '애니메이션 좋아요 취소에 실패�
 
 export const SEARCH_INPUT = '작품, 캐릭터를 검색해보세요.';
 export const REQUIRE_LOGIN = '로그인이 필요합니다.';
+
+export const UNKNOWN_REVIEW_TARGET = '알 수 없는 리뷰 타겟입니다.';
