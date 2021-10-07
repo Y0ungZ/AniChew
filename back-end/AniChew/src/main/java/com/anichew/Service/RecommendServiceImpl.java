eaaac1b66f2aff98c3d871782199b4d50849562f
shining8543 <shining8543@naver.com>
2021-10-07 19:40:41 +0900

    feat:[#S05P21B304-1062 ] Recommend : Favorite
    Former-commit-id: 65484fe1d959ab98d5dcbfe290f59bb19ae092e1

--- back-end/AniChew/src/main/java/com/anichew/Service/RecommendServiceImpl.java
+++ back-end/AniChew/src/main/java/com/anichew/Service/RecommendServiceImpl.java
@@ -12,10 +12,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.anichew.Entity.Anime;
+import com.anichew.Entity.Animescore;
+import com.anichew.Entity.FavoriteAnime;
 import com.anichew.Entity.RecommendStart;
+import com.anichew.Entity.SimilarAnime;
+import com.anichew.Entity.User;
+import com.anichew.Repository.AnimescoreRepository;
 import com.anichew.Repository.FavoriteAnimeRepository;
 import com.anichew.Repository.RecommendStartRepository;
+import com.anichew.Repository.SimilarAnimeRepository;
+import com.anichew.Repository.UserRepository;
 import com.anichew.Response.AnimeResponse;
+import com.anichew.Util.CookieUtil;
+import com.anichew.Util.JwtUtil;
 
 @Service
 public class RecommendServiceImpl implements RecommendService {
@@ -26,6 +35,22 @@ public class RecommendServiceImpl implements RecommendService {
 	
 	@Autowired
 	FavoriteAnimeRepository favoriteAnimeRepo;
+	
+	@Autowired
+	AnimescoreRepository animescoreRepo;
+	
+	@Autowired
+	SimilarAnimeRepository similarAnimeRepo;
+	
+	@Autowired
+	UserRepository userRepo;
+	
+	
+	@Autowired
+	JwtUtil jwtUtil;
+	
+	@Autowired
+	CookieUtil cookieUtil;
 	
 	
 	@Override
@@ -112,30 +137,51 @@ public class RecommendServiceImpl implements RecommendService {
 
 	@Override
 	public List<AnimeResponse> getFromBaseOfFavorite(HttpServletRequest httpServletReq) {
-		List<RecommendStart> recommends = recommendStartRepo.findAll();
-		Set<RecommendStart> set = new HashSet();
+		
+		long userid = cookieUtil.getUserid(httpServletReq, jwtUtil, jwtUtil.ACCESS_TOKEN_NAME);
+		User user = userRepo.findById(userid);	
+		
+		List<FavoriteAnime> favoriteAnimes = user.getFavoriteAnimes();
+		List<Animescore> myWatched = animescoreRepo.findAllByUser(user);
+		
+		Set<Anime> myWatchedSet = new HashSet();
+		Set<Anime> similarSet = new HashSet();
+		Set<Anime> recommendSet = new HashSet();
+		
+		for(Animescore animescore : myWatched) {
+			Anime anime = animescore.getAnime();
+			myWatchedSet.add(anime);
+		}
+		
+		
+		for(FavoriteAnime favoriteAnime : favoriteAnimes) {
+			Anime anime = favoriteAnime.getAnime();
+			List<SimilarAnime> similars = similarAnimeRepo.findAllByAnime(anime);
+			for(SimilarAnime similarAnime : similars) {
+				if(!myWatchedSet.contains(similarAnime.getAnime())) {
+					similarSet.add(similarAnime.getAnime());
+				}
+			}
+		}
 		
 		List<AnimeResponse> response = new ArrayList();
-		
+		List<Anime> animes = new ArrayList(similarSet);
 		Random random = new Random();
 		
-		while(set.size() < 10) {
-			int idx = random.nextInt(305);
-			RecommendStart recommend = recommends.get(idx);
-			
-			
-			if(!set.contains(recommend)) {
-				set.add(recommend);
+		while(recommendSet.size()<10 || recommendSet.size() < animes.size()) {
+			int idx = random.nextInt(animes.size());
+			Anime anime = animes.get(idx);
+			if(!recommendSet.contains(anime)) {
+				recommendSet.add(anime);
 				AnimeResponse animeRes = new AnimeResponse();
-				Anime anime = recommend.getAnime();
 				animeRes.setId(anime.getId());
 				animeRes.setKoreanName(anime.getKoreanName());
 				animeRes.setName(anime.getName());
 				response.add(animeRes);
 			}
-			
-		}		
+		}
 		
+				
 		return response;
 	}
 }
