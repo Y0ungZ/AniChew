52a3f00770abf9a8943e1ec308ddcb1f3cf09068
rolancia <95jjangjun@gmail.com>
2021-10-07 12:06:32 +0900

    feat: [#S05P21B304-1054] move search open state
    Former-commit-id: 42f0aa83199d259ae4a9886085846c19cf2715f0

--- front-end/src/components/header/header-menu.tsx
+++ front-end/src/components/header/header-menu.tsx
@@ -3,14 +3,11 @@ import { Link, useHistory } from 'react-router-dom';
 import { observer } from 'mobx-react';
 import { Avatar, Button, Dropdown, Menu } from 'antd';
 import { SearchOutlined } from '@ant-design/icons';
-import { useAuth, useUser } from 'hooks';
+import { useAuth, useSearch, useUser } from 'hooks';
 import { LoginModal } from 'components';
 import { config } from 'config/config';
 import { CssKeyObject } from 'types/css-basic-type';
-
-type toggleSearchProps = {
-  toggleSearchHeader: () => void;
-};
+import { msg } from 'util/message';
 
 const styles: CssKeyObject = {
   icon: {
@@ -21,20 +18,22 @@ const styles: CssKeyObject = {
   },
 };
 
-const HeaderMenu = observer((props: toggleSearchProps) => {
+const HeaderMenu = observer(() => {
   const history = useHistory();
   const auth = useAuth();
   const user = useUser();
+  const search = useSearch();
   const [visible, setVisible] = useState(false);
 
   const toggleSearchHeader = () => {
-    props.toggleSearchHeader();
+    search.searchOpen = !search.searchOpen;
   };
 
   const logout = () => {
-    localStorage.removeItem('token');
-    auth.logout();
-    history.push('/');
+    auth
+      .logout()
+      .then(() => history.push('/'))
+      .catch((error) => msg('Error', error.message));
   };
 
   const menu = (
